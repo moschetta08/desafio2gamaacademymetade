dd9ed04d6b18dd0cc35308e4812c4d3647960dcc
moschetta08 <60576590+moschetta08@users.noreply.github.com>
2021-07-25 22:44:27 -0300

    variavel de ambiente

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Home from './view/Home'
 
 const App: React.FC = () => {
+  fetch(process.env.REACT_APP_API_URL || 'http://localhost:8080/api/v1')
+    .then(() => console.log('nois'))
+    .catch(() => console.log('fudeu'))
   return <Home />;
     
 }
